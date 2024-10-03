68e2e8296169473049d57216564307a9c53a4a93
imo2k <dnrwk99@gmail.com>
2024-10-03 20:10:30 +0900

    Initial Commit

---
+++ Test_Calculator/Test_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_CalculatorApp.swift
+//  Test_Calculator
+//
+//  Created by GO on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
